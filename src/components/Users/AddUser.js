0cefec65be1233d25220c6d35a3ca464407703fd
jinwoo <jinwoo.jace@gmail.com>
2023-09-24 12:37:21 +0900

    [feat] Split the app into components with css module

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,23 @@
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+import Button from "../UI/Button";
+
+const AddUser = (props) => {
+  const addUserHandler = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
