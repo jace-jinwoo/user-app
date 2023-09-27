dd299009824aad4f48b8eba7f27c7f6930ab0228
jinwoo <jinwoo.jace@gmail.com>
2023-09-27 22:56:48 +0900

    [feat] Manage states / Add a validation

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,19 +1,47 @@
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
+import { useState } from "react";
+
+const initialValue = {
+  userName: "",
+  age: "",
+};
 
 const AddUser = (props) => {
+  const [entered, setEntered] = useState(initialValue);
   const addUserHandler = (e) => {
     e.preventDefault();
+    if (entered.userName.trim().length === 0 || entered.age.trim().length === 0)
+      return;
+    if (entered.age < 1) return;
+
+    console.log(entered);
+    setEntered(initialValue);
+  };
+
+  const changeHandler = (e) => {
+    const { id, value } = e.target;
+
+    setEntered({ ...entered, [id]: value });
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" />
+        <input
+          id="userName"
+          value={entered.userName}
+          onChange={changeHandler}
+        />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <input
+          id="age"
+          type="number"
+          value={entered.age}
+          onChange={changeHandler}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
