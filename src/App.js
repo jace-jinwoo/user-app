5ebf5408272d906d6ecb9947a35c6f4dd7da6a84
jinwoo <jinwoo.jace@gmail.com>
2023-09-30 00:19:58 +0900

    [feat] Manage global state using Context API

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import React, { useState } from "react";
 import AddUser from "./components/Users/AddUser";
 import UserList from "./components/Users/UserList";
 import AddUserValidation from "./components/Users/AddUserValidation";
+import AuthContext from "./store/auth-context";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
   const [userList, setUserList] = useState([]);
+
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "SECRET_KEY");
+    setIsLoggedIn(true);
+  };
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
 
   const addUserHandler = (uName, uAge) => {
     setUserList((prevUserList) => {
@@ -16,11 +27,11 @@ function App() {
   };
 
   return (
-    <div>
+    <AuthContext.Provider value={{ isLoggedIn, onLogout: logoutHandler }}>
       <AddUserValidation onAddUser={addUserHandler} />
       {/* <AddUser onAddUser={addUserHandler} /> */}
       <UserList users={userList} />
-    </div>
+    </AuthContext.Provider>
   );
 }
 
--- src/components/Users/UserList.js
+++ src/components/Users/UserList.js
@@ -1,7 +1,13 @@
+import { useContext } from "react";
+import AuthContext from "../../store/auth-context";
 import Card from "../UI/Card";
 import classes from "./UserList.module.css";
 
 const UserList = (props) => {
+  const authContext = useContext(AuthContext);
+
+  console.log("authContext :: ", authContext);
+
   return (
     <Card className={classes.users}>
       <ul>
